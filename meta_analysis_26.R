ec18a92be4ba2e23af1678064f67d0a1c3c115a8
tran986 <tran.986@buckeyemail.osu.edu>
2026-07-20 08:05:57 -0400

    pipeline1+2 done on HPC

--- meta_analysis_26.R
+++ meta_analysis_26.R
@@ -1,3 +1,4 @@
+library(TreeSummarizedExperiment)
 library(dplyr)
 library(tidyverse)
 library(readxl)
@@ -5,75 +6,9 @@ library(readr)
 library(purrr)
 library(dplyr)
 library(tidyverse)
-library(gdata)
 library(devtools)
-devtools::load_all("/Users/tran.986/Desktop/phylogenize/package/repermulize")
-devtools::load_all("/Users/tran.986/Desktop/phylogenize/package/phylogenize")
-
-md = read_excel("~/Desktop/meta_analysis_26/metadata_with_cond.xls",
-                          sheet = 1) # use this as a standard (FROM THE PAPER)
-#remove T1D:
-md = md[md$Status != "T1D", ]
-View(md)
-#---------Chinese dataset:
-md_CHNs = read_csv("~/Desktop/meta_analysis_26/metadata_CHNs.csv") #CHNs only
-#merge info to obtain sequence list for wget downloads:
-md_CHNs["Sample"]<-substring(md_CHNs$`Sample Name`, 5)
-CHNs_md_final = md %>% left_join(md_CHNs, by = "Sample") %>%
-  filter(!is.na(Run))
-write.csv(CHNs_md_final, "~/Desktop/meta_analysis_26/metadata_CHNs_final.csv")
-
-#---------MHN dataset1:--Study: ERP004605
-md_mhn1 = read_tsv("~/Desktop/meta_analysis_26/filereport_MHN_1.tsv")
-
-md_mhn1$Sample <- str_extract(md_mhn1$submitted_ftp, "[^/]+$") %>%
-  str_extract("[^/]+$") %>%   # keep after last /
-  str_extract("^[^_]+") %>% #keep before _
-  str_extract("^[^-]+")  
-
-mhn1_md_final = md_mhn1 %>%
-  left_join(md, by = c("Sample")) %>%
-  filter(!is.na(Status)) %>%
-  select(Sample, `Country subset`, Status, fastq_ftp)
-write.csv(mhn1_md_final, "~/Desktop/meta_analysis_26/mhn1_md_final.csv")
-
-#---------MHN dataset2: - only have ND Ctrl: --Study: ERP003612
-md_mhn2 = read_tsv("~/Desktop/meta_analysis_26/filereport_MHN_2.tsv")
-md_mhn2$Sample <- str_extract(md_mhn2$submitted_ftp, "[^/]+$") %>%
-  str_extract("[^/]+$") %>%   # keep after last /
-  str_extract("(?<=MetaHIT-)[^_]+")
-
-mhn2_md_final = md_mhn2 %>% 
-  left_join(md, by = c("Sample")) %>% 
-  filter(!is.na(Status)) %>%
-  select(Sample, `Country subset`, Status, fastq_ftp)
-write.csv(mhn2_md_final, "~/Desktop/meta_analysis_26/mhn2_md_final.csv")
-
-#---------MHN dataset3: --Study: ERP002469 
-md_mhn3 = read_tsv("~/Desktop/meta_analysis_26/filereport_MHN_3.tsv")
-md_mhn3$Sample <- str_extract(md_mhn3$submitted_ftp, "[^/]+$") %>%
-  str_extract("[^/]+$") %>%
-  str_extract("^[^_]+_[^_]+")
-
-mhn3_md_final = md_mhn3 %>% 
-  left_join(md, by = c("Sample")) %>% 
-  filter(!is.na(Status)) %>%
-  select(Sample, `Country subset`, Status, fastq_ftp)
-write.csv(mhn3_md_final, "~/Desktop/meta_analysis_26/mhn3_md_final.csv")
-
-
-#--------MHN dataset4: --Study: ERP002061 -- all ND CTRL
-md_mhn4 = read_tsv("~/Desktop/meta_analysis_26/filereport_MHN_4.tsv")
-md_mhn4$Sample <- str_extract(md_mhn4$submitted_ftp, "[^/]+$") %>%
-  str_extract("[^/]+$") %>%
-  str_extract("^[^_]+") %>% #keep before _
-  str_extract("^[^-]+")
-
-mhn4_md_final = md_mhn4 %>% 
-  left_join(md, by = c("Sample")) %>% 
-  filter(!is.na(Status)) %>%
-  select(Sample, `Country subset`, Status, fastq_ftp)
-write.csv(mhn4_md_final, "~/Desktop/meta_analysis_26/mhn4_md_final.csv")
+#devtools::load_all("/fs/project/bradley.720/projects/phylogenize_v2/phylogenize_repermulize/package/repermulize")
+devtools::load_all("/fs/project/bradley.720/projects/phylogenize_v2/phylogenize_repermulize/package/phylogenize")
 
 
 #--------
@@ -82,7 +17,7 @@ write.csv(mhn4_md_final, "~/Desktop/meta_analysis_26/mhn4_md_final.csv")
 #e.g., study_id = CHN, ERP004605, ERP003612, ERP002061, ERP002469
 wget_urls_func <- function(md_final, study_id) {
   
-  out_file <- paste0("~/Desktop/meta_analysis_26/fastq_url/", study_id, "_fastq_urls.txt")
+  out_file <- paste0("/fs/project/bradley.720/projects/meta_analysis_26/fastq_url/", study_id, "_fastq_urls.txt")
   
   wget_urls <- c(
     paste0(
@@ -103,6 +38,28 @@ wget_urls_func <- function(md_final, study_id) {
   
   writeLines(wget_urls, out_file)
 }
+
+#function that run full function 2, 3 and 4:
+phylogenize_full_func=function(study_id, metadata_dir_path, envs_compared, ref_env) {
+  #import bracken:
+  bracken_out = import_bracken(study_id = study_id)
+  
+  #extract phyloz_metadata:
+  metadata = read_csv(metadata_dir_path)
+  ext_md = extract_phyloz_metadata(import_bracken_out = bracken_out,
+                                   metadata = metadata,
+                                   study_id = study_id,
+                                   envs_compared = envs_compared)
+  
+  #run phylogenize to get .rds:
+  phylogenize_run(#extract_phyloz_metadata_out = ext_md,
+    #import_bracken_out = bracken_out,
+    study_id = study_id,
+    ref_env = ref_env,
+    phenotype = "abundance")
+  
+}
+
 
 #2. function to read into .bracken files (push this to ASC):
 import_bracken=function(study_id) { 
@@ -132,16 +89,16 @@ extract_phyloz_metadata=function(import_bracken_out,
     
   {
     metadata_clean=metadata[,c("Run", "Status")] %>%
-      rename("env"="Status",
-             "sample"="Run") %>%
-      mutate(dataset = study_id)
+      dplyr::rename("env"="Status",
+                    "sample"="Run") %>%
+      dplyr::mutate(dataset = study_id)
     
   } else { #all MHN studies with "ERR" samples
     metadata_clean = metadata %>% 
-      mutate(sample = str_extract(fastq_ftp, "(?<=/)[^/]+(?=/[^/]+\\.fastq\\.gz)"),
-             dataset = study_id) %>%
-      select(Status, sample, dataset) %>%
-      rename("env"="Status")
+      dplyr::mutate(sample = str_extract(fastq_ftp, "(?<=/)[^/]+(?=/[^/]+\\.fastq\\.gz)"),
+                    dataset = study_id) %>%
+      dplyr::select(Status, sample, dataset) %>%
+      dplyr::rename("env"="Status")
   }
   
   #filter 2 envs that will be compared:
@@ -155,35 +112,53 @@ extract_phyloz_metadata=function(import_bracken_out,
 
 #4. apply phylogenize on inputs: metadata and count
 dataset_dir = "/fs/project/bradley.720/projects/phylogenize_v2/phylogenize/package/phylogenize/inst/extdata"
-phylogenize_run=function(extract_phyloz_metadata_out,
-                         import_bracken_out,
-                         study_id,
+phylogenize_run=function(provided_file_path = NULL,
+                         #extract_phyloz_metadata_out,
+                         #import_bracken_out,
+                         study_id = NULL,
+                         phenotype, #"abundance" or "provided"
                          ref_env) { #"ND CTRL or T2D metformin-
-  
-  res=phylogenize(
-    output_file = paste0(working_dir, "/phylogenize_out/", study_id, "/phylogenize.html"),
-    out_dir = paste0(working_dir, "/phylogenize_out/", study_id),
-    data_dir = dataset_dir,
-    db = "human-gut",
-    taxon_level = "family",
-    type_16S=F,
-    which_phenotype = "abundance",
-    diff_abund_method = "ancombc2",
-    abundance_file = import_bracken_out,
-    metadata_file = extract_phyloz_metadata,
-    which_envir = ref_env, 
-    env_column = "env",
-    sample_column = "sample",
-    ncl=parallelly::availableCores(),
-    core_method = "permutrate-rlm")
-  
-  saveRDS(res, 
-          paste0(working_dir, "/phylogenize_out/", study_id, "/phylogenize.rds"))
+  if (phenotype == "abundance") {
+    res=phylogenize(
+      output_file = paste0(working_dir, "/phylogenize_out/", study_id, "/phylogenize.html"),
+      out_dir = paste0(working_dir, "/phylogenize_out/", study_id),
+      data_dir = dataset_dir,
+      db = "human-gut",
+      taxon_level = "family",
+      type_16S=F,
+      which_phenotype = phenotype,
+      diff_abund_method = "ancombc2",
+      abundance_file = paste0(working_dir, "/phylogenize_out/", study_id, "/data_w_count.tsv"),
+      metadata_file = paste0(working_dir, "/phylogenize_out/", study_id, "/metadata_filter.tsv"),
+      which_envir = ref_env, 
+      env_column = "env",
+      sample_column = "sample",
+      ncl=parallelly::availableCores())
+    
+  } else { #provided phenotype
+    res = phylogenize(
+      output_file = paste0(working_dir, "/pipeline2/phylogenize/out/phylogenize.html"),
+      out_dir = paste0(working_dir, "/pipeline2/phylogenize/out"),
+      data_dir = dataset_dir,
+      db = "human-gut",
+      taxon_level = "family",
+      which_phenotype = "provided",
+      diff_abund_method = "ancombc2",
+      phenotype_file = provided_file_path,
+      abundance_file = paste0(working_dir, "/phylogenize_out/CHN/data_w_count.tsv"),
+      metadata_file = paste0(working_dir,"/phylogenize_out/CHN/metadata_filter.tsv"),
+      which_envir = ref_env,
+      env_column = "env",
+      sample_column = "sample",
+      ncl = parallelly::availableCores())
+    
+  }
+  saveRDS(res, paste0(working_dir, "/phylogenize_out/", study_id, "/phylogenize.rds"))
   return(res)
 }
 
-#5. Function to run the other pipeline (pre-phylogenize merging pipeline):
-#from bracken output -> apply ancombc:
+#PIPELINE2: function for pre-phylogenize merging:
+#5. function to run ancombc inputs separately:
 ancomRun = function(count_tbl, study_id, metadata){
   
   count = count_tbl %>% column_to_rownames(var = "name")
@@ -191,11 +166,10 @@ ancomRun = function(count_tbl, study_id, metadata){
                                          metadata = metadata,
                                          study_id = study_id,
                                          envs_compared = c("ND CTRL", "T2D metformin-"))
-  metadata_tbl = metadata_tbl %>% column_to_rownames(var = "sample")
+  metadata_tbl = metadata_tbl %>% column_to_rownames(var = "sample") 
   
   #filter out those samples in the metadata but not in count_tbl:
   count_tbl_clean = count %>% dplyr::select(rownames(metadata_tbl))
-  all(colnames(count_tbl_clean) == rownames(metadata_tbl))
   tse <- TreeSummarizedExperiment(assays = S4Vectors::SimpleList(counts = as.matrix(count_tbl_clean)),
                                   colData = S4Vectors::DataFrame(metadata_tbl)
   )
@@ -205,10 +179,120 @@ ancomRun = function(count_tbl, study_id, metadata){
                               assay_name = "counts",
                               fix_formula = "env")
   
+  saveRDS(res_ancom,
+          paste0(working_dir, "/pipeline2/ancom/", study_id, "_ancombc_res.rds"))
+  
   return(list(res = res_ancom,
               count_input = count_tbl_clean,
               metadata_input = metadata_tbl))
-  saveRDS(res_ancom,
-          paste0(working_dir, "/pipeline2/ancom/", study_id, "_ancombc_res.rds"))
 }
+
+
+#6. function to calculate combined mu and combined se:
+#input is a list of ashr output of all study_id:
+#taking both ancombc output and ashr outputs (parameter: ash_used = T/F)
+#formula to compute for combined effect size: -for 1 study_id first
+weightCal=function(SE) { #input: se of 1 study_id only 
+  weight_per_study = 1/(SE**2)
+  return(weight_per_study)  
+}
+
+#a function that loops through ancom_res of each study_id and extract for mu and se:
+ancomExtract=function(ancomRun_output, 
+                      envs_compared = c("ND CTRL",
+                                        "T2D metformin-")) { #of 1 study_id only
+  if (identical(envs_compared, c("ND CTRL", "T2D metformin-"))) {
+    mu_se_df=data.frame(taxon = ancomRun_output$res$taxon,
+                        mu=ancomRun_output$res$`lfc_envT2D metformin-`,
+                        se=ancomRun_output$res$`se_envT2D metformin-`)}
+  mu_se_df
+}
+
+
+weight_per_taxonCal=function(ancomRun_output_ls, study_id_ls){
+  weight_per_taxon=lapply(ancomRun_output_ls, function(x) { #per study here:
+    mu_se_df=ancomExtract(ancomRun_output = x,
+                          envs_compared = c("ND CTRL", "T2D metformin-"))
+    weight_per_taxon=data.frame(taxon=mu_se_df$taxon,
+                                weight=weightCal(SE = mu_se_df$se),
+                                mu = mu_se_df$mu)
+    weight_per_taxon #return weight (per_taxon) for each study_id in a list
+  })
+  names(weight_per_taxon)<-study_id_ls
+  return(weight_per_taxon) #should be a ls of df with 2 columns - taxon and each of their weight
+  
+}
+
+sigma_weightCal <- function(weight_per_taxon_ls) {
+  # combine list of dfs into one long df, tagging study
+  combined <- purrr::imap_dfr(weight_per_taxon_ls, ~ dplyr::mutate(.x, study = .y))
+  
+  # sum weights per taxon across studies (NA-safe: taxa absent in a study just don't contribute)
+  combine_df = combined %>% group_by(taxon) %>% summarise(sum_weight = sum(weight, na.rm = TRUE), 
+                                                          n_studies  = n(),  # how many studies contributed to this taxon
+                                                          .groups = "drop")
+  combine_df
+}
+
+
+#loop throgh all studyId and apply weightCal--> to get sigma_weightCal_out
+SE_poolCal = function(sigma_weightCal_out) { #input: sum of all weigtCal_out
+  sigma_weightCal_out["se_pooled"] <- sqrt(1/sigma_weightCal_out$sum_weight)
+  return(sigma_weightCal_out)
+}
+
+sum_prod_weightCal = function(weight_per_taxon_ls,
+                              study_id_ls) 
+{
+  prod_ls=lapply(weight_per_taxon_ls, function(i) {
+    i["prod_weight_mu"]<- i$mu * i$weight
+    i 
+  })				      
+  names(prod_ls)<-study_id_ls
+  combined <- purrr::imap_dfr(prod_ls, ~ dplyr::mutate(.x, study = .y))
+  combined %>% 
+    dplyr::group_by(taxon) %>%
+    dplyr::summarise(
+      sum_prod_weight_mu = sum(prod_weight_mu, na.rm = T),
+      n_studies = n(),
+      .groups = "drop")
+}
+
+mu_poolCal = function(sum_prod_weightCal_out,
+                      sigma_weightCal_out) { #in = sum of all w (from weightCal)
+  df_comb = inner_join(sum_prod_weightCal_out, sigma_weightCal_out,
+                       by = "taxon")
+  df_comb["mu_pooled"] = df_comb$sum_prod_weight_mu / df_comb$sum_weight
+  df_comb=df_comb %>% dplyr::select(taxon, 
+                                    sum_prod_weight_mu,
+                                    n_studies.x,
+                                    sum_weight,
+                                    mu_pooled
+  )
+  return(df_comb)
+}
+
+
+#7. function to run ashr:- clean taxon name:
+#works only for 1 study_id: all_ancom_res[[1]]
+ashRun = function(mu_poolCal_out, SE_poolCal_out) {
+  #combine 2 inputs so the taxon matched:
+  interDf=inner_join(mu_poolCal_out, SE_poolCal_out, by = "taxon") %>%
+    dplyr::select(taxon, n_studies, se_pooled, mu_pooled)
+  bh = interDf$mu_pooled
+  sebh = interDf$se_pooled
+  
+  ash_run = ashr::ash(betahat = bh,
+                      sebetahat = sebh)
+  ash_res = ash_run$result
+  
+  #attach taxon name from ancom to ash res:
+  res_tbl = cbind(interDf$taxon,
+                  ash_res)
+  #rename taxon column:
+  colnames(res_tbl)[1]<-"taxon"
+  return(res_tbl)
+  
+}
+
 
