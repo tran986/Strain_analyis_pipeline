ff1a10c066bb3a19e4f4ee35d7c488926c2212eb
tran986 <tran.986@buckeyemail.osu.edu>
2026-08-21 09:06:51 -0400

    fix taxamerge_done

--- meta_analysis_RUN_TaxaMerge.R
+++ meta_analysis_RUN_TaxaMerge.R
@@ -14,177 +14,79 @@ source(paste0(working_dir, "/meta_analysis_26.R"))
 #study 2 count_tbl + metadata --> ANCOMBC --> ashr
 #...do for all of the studies --> merge (combined) at taxa signals --> Phylogenize2
 
-#============================================================#======================================= Pipeline 1 merge at taxa level:
-#--step 1: run ancombc2
-#import_bracken_CHN=read_tsv(paste0(working_dir, "/phylogenize_out/CHN/data_w_count.tsv"))
-#import_bracken_MH1=read_tsv(paste0(working_dir, "/phylogenize_out/ERP004605_MH1/data_w_count.tsv"))
-#import_bracken_MH3=read_tsv(paste0(working_dir, "/phylogenize_out/ERP002469_MH3/data_w_count.tsv"))
-#import_bracken_MCA=read_tsv(paste0(working_dir, "/pipeline1/phylogenize_out/MCA/data_w_count.tsv"))
-#import_bracken_SKK = read_tsv(paste0(working_dir, "/pipeline1/phylogenize_out/SKK/data_w_count.tsv"))
+#==================================================MERGE count table to run ANCOMBC:
+#ource("/fs/project/bradley.720/projects/meta_analysis_26/meta_analysis_26.R")
+#working_dir = "/fs/project/bradley.720/projects/meta_analysis_26"
 
-#count_tbl_ls = list(MCA = import_bracken_MCA,
-#		    SKK = import_bracken_SKK,
-#		    CHN = import_bracken_CHN,
-#	       	    ERP004605_MH1 = import_bracken_MH1,
-#	       	    ERP002469_MH3 = import_bracken_MH3)
+#---merge all count table from all datasets:
+#import_ls = c("CHN", "ERP004605_MH1", "ERP002469_MH3", "MCA", "SKK")
+#import_bracken_ls = setNames(lapply(import_ls, function(i) {
+#  read_tsv(paste0(working_dir, "/mergeGene/phylogenize_out/", i, "/data_w_count.tsv"))
+#}), import_ls)
 
-#metadata_tbl_ls = list(MCA = read_csv(paste0(working_dir, "/metadata/MCA_md_final.csv")),
-#		       SKK = read_csv(paste0(working_dir, "/metadata/SKK_md_final.csv")),
-#		       CHN = read_csv(paste0(working_dir,"/metadata/CHN_md_final.csv")),
-#		       ERP004605_MH1 =  read_csv(paste0(working_dir,"/metadata/mhn1_md_final.csv")),
-#		       ERP002469_MH3 = read_csv(paste0(working_dir,"/metadata/mhn3_md_final.csv")))
+#count_tbl_merge = Reduce(function(x,y) merge(x, y, by = "name", all = T), import_bracken_ls)
 
-study_id_ls = c("CHN", "ERP004605_MH1", 
-                "ERP002469_MH3")
-
-
-#-------------------run through all of the items in the list:
-#all_ancom_res <- lapply(seq_along(study_id_ls), function(i) {
-#	cat("\nRunning", study_id_ls[i], "\n")
-#       ancomRun(count_tbl = count_tbl_ls[[i]],
-#	study_id = study_id_ls[i],
-#	metadata = metadata_tbl_ls[[i]])
+#count_tbl_merge[-1]<- lapply(count_tbl_merge[-1], function(x){
+#  x_num <- suppressWarnings(as.numeric(as.character(x)))
+#  x_num[is.na(x_num) & !is.na(x)] <- 0
+#  x_num
 #})
 
-#names(all_ancom_res) <- study_id_ls
+#count_tbl_merge[is.na(count_tbl_merge)] <- 0
+#print("any non-numeric value:")
+#anyNA(count_tbl_merge[-1])
 
-#saveRDS(all_ancom_res,
-#	paste0(working_dir, "/pipeline2/ancom/all_ancom_res.rds"))
 
-#---step 2:--------------------combine ancombc w fixed effect?
-#ancom_out_ls = lapply(study_id_ls, function(id) 
-#       readRDS(paste0(working_dir, "/pipeline2/ancom/", id, "_ancombc_res.rds"))
-#       )
-#--calculate weight per taxon for each study_id:
-#weight_per_taxon=weight_per_taxonCal(ancomRun_output_ls = ancom_out_ls,
-#		    study_id_ls = study_id_ls)
-
-#--calculate the sum of weights per taxon across studies:
-#sigma_weight_per_taxon=sigma_weightCal(weight_per_taxon_ls = weight_per_taxon)
-#write.csv(sigma_weight_per_taxon, paste0(working_dir, "/pipeline2/ancom/combine/sigma_weight.csv"))
-
-#--calculate the SE pooled:
-#se_pooled=SE_poolCal(sigma_weightCal_out = sigma_weight_per_taxon)
-#write.csv(se_pooled, paste0(working_dir, "/pipeline2/ancom/combine/se_pooled.csv"))
-
-#--for each study_id, 
-#--calculate the product of weight (per_taxon) * effect_size (of that taxon) 
-#sum_prod_weight_mu=sum_prod_weightCal(weight_per_taxon_ls = weight_per_taxon,
-#				      study_id_ls = study_id_ls)
-#write.csv(sum_prod_weight_mu, paste0(working_dir, "/pipeline2/ancom/combine/sum_prod_weight_mu.csv"))
-
-#--calculate for pooled effect size
-#eff_size_pooled = mu_poolCal(sum_prod_weightCal_out = sum_prod_weight_mu, 
-#			     sigma_weightCal_out = sigma_weight_per_taxon)
-#write.csv(eff_size_pooled, paste0(working_dir, "/pipeline2/ancom/combine/eff_size_pooled.csv"))
-
-#---step 3a: run ashr only once on the combined effect 
-#ash_res_pooled=ashRun(mu_poolCal_out = eff_size_pooled,
-#       SE_poolCal_out = se_pooled)
-#write.csv(ash_res_pooled, paste0(working_dir, "/pipeline2/ash/ash_res_pooled.csv"))
-
-#---step 3b: run phylogenize repermulize()
-#preping for provided phenotype file:
-#se_poolCal_out = read.csv(paste0(working_dir,"/pipeline2/ancom/combine/se_pooled.csv"))
-#mu_poolCal_out = read.csv(paste0(working_dir, "/pipeline2/ancom/combine/eff_size_pooled.csv"))
-
-#use ashr result as a provided_file instead:
-#ash_res_pooled = read.csv(paste0(working_dir, "/pipeline2/ash/ash_res_pooled_5studies.csv"))
-
-#provided_file=inner_join(mu_poolCal_out, se_poolCal_out, 
-#                         by = "taxon")  %>%
-#  dplyr::filter(n_studies == 3) %>%
-#  dplyr::select(taxon, se_pooled, mu_pooled) %>% 
-#  dplyr::rename("estimate"="mu_pooled",
-#                "stderr"="se_pooled")
-
-#write.table(provided_file, 
-#            paste0(working_dir, "/pipeline2/phylogenize/input/phenotype_file.tsv"),
+#write.table(count_tbl_merge, 
+#            file = paste0(working_dir, "/mergeTaxa/ancom/combine_fix/count_tbl_merged.tab"),
 #            sep = "\t",
-#            row.names = F, 
+#            row.names = F,
 #            quote = F)
 
-#phylogenize_run(provided_file = paste0(working_dir,
-#                                       "/pipeline2/phylogenize/input/phenotype_file.tsv"),
-#                study_id =NULL,
-#                phenotype = "provided",
-#                ref_env = c("T2D metformin-"))
+#--merge all metadata
+metadata_ls=setNames(lapply(import_ls, function(i) {
+  df = read_tsv(paste0(working_dir, "/mergeGene/phylogenize_out/", i, "/metadata_filter.tsv")) |> dplyr::mutate(dataset = i)
+}), import_ls)
+
+metadata_tbl_merge = bind_rows(metadata_ls) |> dplyr::select(sample, env, dataset)
+write.table(metadata_tbl_merge, 
+            file = paste0(working_dir, "/mergeTaxa/ancom/combine_fix/metadata_merged.tab"),
+            sep = "\t", row.names = FALSE)
+
+#--run ANCOMBC:
+#ancomMerge = ancomRun(count_tbl = count_tbl_merge,
+#	 study_id = NULL,
+#	 metadata = metadata_tbl_merge)
+
+ancomMerge = readRDS(paste0(working_dir, "/mergeTaxa/ancom/combine_fix/merged_ancom_res.rds"))
+
+#--run ashr:
+print("fitting ashr")
+ancomMerge_res = ancomMerge$res
+ashMerge = ashr::ash(betahat = ancomMerge_res$`lfc_envT2D metformin-`,
+                     sebetahat = ancomMerge_res$`se_envT2D metformin-`) 
+
+#rename ash res to become a "provided" file for phylogenize
+ashMerge_res = ashMerge$res
+ashMerge_res$taxon <- ancomMerge_res$taxon
+ashMerge_res = ashMerge_res %>%
+  dplyr::select(taxon, PosteriorMean, PosteriorSD) %>%
+  dplyr::rename("estimate"="PosteriorMean",
+                "stderr"="PosteriorSD")
+
+write.table(ashMerge_res,
+            paste0(working_dir, "/mergeTaxa/ash/ashMerge_res.tab"),            
+            sep = "\t", row.names = FALSE)   #this is "provided" file 
+
+#--run phylogenize2:
+phylogenize_run(provided_file_path = paste0(working_dir, "/mergeTaxa/ash/ashMerge_res.tab"),
+                study_id = NULL,
+                phenotype = "provided",
+                ref_env = "ND CTRL")
 
 
 
-study_ids = c("CHN", "ERP004605_MH1", "ERP002469_MH3", "MCA", "SKK")
-
-#read into list of ancombc results + ashr 
-ancombc_list = setNames(lapply(study_ids, function(id) {
-  
-  res = readRDS(paste0(working_dir, "/ancom/", id, "_ancombc_res.rds"))
-  #run ancombc
-  extracted = ancomExtract(ancomRun_output = res)
-  #run ashr:
-  ash_fit = ash(betahat = extracted$mu, sebetahat = extracted$se)
-  
-  extracted$lfc_shrunk = ash_fit$result$PosteriorMean
-  extracted$se_shrunk  = ash_fit$result$PosteriorSD
-  
-  extracted$study<-id
-  extracted %>% select(taxon, lfc_shrunk, se_shrunk, study)
-  
-}), study_ids)
-
-combined_ancombc = bind_rows(ancombc_list)
 
 
-
-
-
-
-
-
-
-
-
-
-
-#create a provided_file where n_studies >= 4
-provided_file = taxon_meta_results %>%
-  dplyr::filter(n_studies == 5) |>
-  dplyr::select(taxon, se_pooled, theta_pooled) |>
-  dplyr::rename("estimate"="theta_pooled",
-                "stderr"="se_pooled") %>% 
-  drop_na(stderr, estimate)
-
-write.table(provided_file, 
-            paste0(working_dir, "/provided_file_REML_w_ash.tsv"),
-            sep = "\t",
-            row.names = F, 
-            quote = F)
-
-#phylogenize is run on HPC:...
-
-#read into phylogenize results of merged 5 studies, w ash, REML:
-all_res_reml5= readRes_n_pdCal(study_id = "ash-5studies-REML") |> #506 hits in total
-  mutate(neglog10q = -log10(q.value),
-         sig_status = case_when(q.value < 0.05 & effect.size > 0 ~ "Up, significant",
-                                q.value < 0.05 & effect.size < 0 ~ "Down, significant"),
-         plot_color = ifelse(neglog10q >= -log10(0.05), 
-                             as.character(taxon),
-                             "not significant"))
-
-pipeline2_plot=ggplot(all_res_reml5, aes(x = effect.size, y = neglog10q, color = taxon)) +
-  geom_point(alpha = 0.65, size = 1.9) +
-  geom_vline(xintercept = 0, color = "black", linetype = "dashed", linewidth = 0.8) +
-  geom_hline(yintercept = -log10(0.05), linetype = "dashed", color = "red") +
-  labs(x = "Effect size", y = expression(-log[10](q)), title = "Pipeline 2: gene associations by taxon across 5 studies (n=506)") +
-  theme_minimal(base_size = 12) +
-  theme(legend.position = "right")
-
-ggsave(plot = pipeline2_plot,
-       filename = paste0(working_dir, "/test_pipeline2_plot.png"),
-       width = 9,
-       height = 5,
-       units = "in",
-       dpi = 600)
-
-study_ids = c("CHN", "ERP004605_MH1", "ERP002469_MH3", "MCA", "SKK")
 
 
